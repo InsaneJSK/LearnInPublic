7028b7c841a49f103e8560797fce0407eb9fea45
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-08-17 16:49:32 +0530

    python-2048-2

--- Python/2048/2048.py
+++ Python/2048/2048.py
@@ -2,10 +2,14 @@ import random, os
 from pynput import keyboard
 from pynput.keyboard import Key
 
+#This is our game board
 lis = [[" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "]]
 
 #Functions
 def board():
+    """
+    Printing the game board
+    """
     for i in lis:
         for j in i:
             print("|", end = "")
@@ -13,6 +17,9 @@ def board():
         print()
 
 def selector():
+    """
+    This code will select random X-Y coordinates in order to choose the place for putting the randomly generated number
+    """
     varx = random.randint(0, 4)
     vary = random.randint(0, 4)
     if lis[vary][varx] == " ":
@@ -21,6 +28,9 @@ def selector():
         selector()
 
 def num():
+    """
+    This code will use random.randint to choose whether to add 2 or 4 on the selected space
+    """
     var = random.randint(1, 2)
     if var == 1:
         return 2
@@ -28,6 +38,9 @@ def num():
         return 4
 
 def Check():
+    """
+    This code will check if you won or lost the game
+    """
     boole = True
     for i in lis:
         for j in i:
@@ -41,6 +54,9 @@ def Check():
         quit()
 
 def Down():
+    """
+    This code will be moving all the numbers and merging downwards
+    """
     for i in range(4):
         for j in range(5):
             if lis[i][j] != " " and lis[i+1][j] == " ":
@@ -62,6 +78,9 @@ def Down():
     Check()
 
 def Up():
+    """
+    This code will be moving all the numbers and merging upwards
+    """
     for i in range(4, 0, -1):
         for j in range(4, -1, -1):
             if lis[i][j] != " " and lis[i-1][j] == " ":
@@ -80,12 +99,51 @@ def Up():
     Check()
 
 def Right():
-    pass
+    """
+    This code will be moving all the numbers and merging towards right
+    """
+    for i in range(5):
+        for j in range(4):
+            if lis[i][j] != " " and lis[i][j+1] == " ":
+                lis[i][j], lis[i][j+1] = lis[i][j+1], lis[i][j]
+    for i in range(4, -1, -1):
+        for j in range(4, 0, -1):
+            if lis[i][j] == lis[i][j-1] and lis[i][j] != " ":
+                lis[i][j], lis[i][j-1] = 2*lis[i][j], " "
+    for i in range(5):
+        for j in range(4):
+            if lis[i][j] != " " and lis[i][j+1] == " ":
+                lis[i][j], lis[i][j+1] = lis[i][j+1], lis[i][j]
+    selector()
+    os.system("cls")
+    board()
+    Check()
 
 def Left():
-    pass
+    """
+    This code will be moving all the numbers and merging towards left
+    """
+    for i in range(4, -1, -1):
+        for j in range(4, 0, -1):
+            if lis[i][j] != " " and lis[i][j-1] == " ":
+                lis[i][j], lis[i][j-1] = lis[i][j-1], lis[i][j]
+    for i in range(5):
+        for j in range(4):
+            if lis[i][j] == lis[i][j+1] and lis[i][j] != " ":
+                lis[i][j], lis[i][j+1] = 2*lis[i][j], " "
+    for i in range(4, -1, -1):
+        for j in range(4, 0, -1):
+            if lis[i][j] != " " and lis[i][j-1] == " ":
+                lis[i][j], lis[i][j-1] = lis[i][j-1], lis[i][j]
+    selector()
+    os.system("cls")
+    board()
+    Check()
 
 def on_key_release(key):
+    """
+    This will use pynput to determine if the user pressed left right up or down
+    """
     if key == Key.right:
         Right()
     elif key == Key.left:
@@ -94,8 +152,21 @@ def on_key_release(key):
         Up()
     elif key == Key.down:
         Down()
+    elif key == Key.esc:
+        try:
+            quit()
+        except KeyboardInterrupt:
+            print("Exiting the game")
 
 #main
+print("""
+      Welcome!
+      This is the classic game 2048
+      in python!!!
+      Commands are simple, use the arrow keys
+      If you wanna quit, press escape
+      Enjoy
+      """)
 selector()
 board()
 print()
