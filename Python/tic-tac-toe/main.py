c778d60b1d5ddd24ebfff7626e17852e6b9a4237
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-08-17 20:05:01 +0530

    python-tic-tac-toe-2

--- Python/tic-tac-toe/main.py
+++ Python/tic-tac-toe/main.py
@@ -24,43 +24,84 @@ def player_input():
     """
     This will take the marker the player will use in the whole game
     """
-    pass
+    marker = 'AXAXAXAXAZAZA'
+
+    while marker not in ('x' or 'o'):
+        marker = input('Player 1: Do you want to be x or o? ').upper()
+        if marker == 'X':
+            return ('X', 'O')
+        elif marker == 'O':
+            return ('O', 'X')
+
+def place_marker(board, marker, pos):
+    """
+    This one will add a marker into your desired position.
+    """
+    board[pos] = marker
+
+def win_check(board, mark):
+    """
+    This one will check wether you won or not
+    """
+    return ((board[1] == board[2] == board[3] == mark) or
+            (board[4] == board[5] == board[6] == mark) or
+            (board[7] == board[8] == board[9] == mark) or
+            (board[1] == board[4] == board[7] == mark) or
+            (board[2] == board[5] == board[8] == mark) or
+            (board[3] == board[6] == board[9] == mark) or
+            (board[7] == board[5] == board[3] == mark) or
+            (board[9] == board[5] == board[1] == mark))
 
 def choose_first():
     """
     This one will decide that who will go first
     """
-    pass
+    number = random.randint(0, 1)
 
-def player_choice():
-    """
-    This one will ask you that where do you want to place your marker
-    """
-    pass
+    if number == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
 
-def full_board_check():
+def space_check(board, pos):
+    """
+    This one will check that is there any space at the position you chose.
+    """
+    return board[pos] == ' '
+
+def full_board_check(board):
     """
     This one will check for a tie.
     """
-    pass
+    return (board[1] != ' ' and
+            board[2] != ' ' and
+            board[3] != ' ' and
+            board[4] != ' ' and
+            board[5] != ' ' and
+            board[6] != ' ' and
+            board[7] != ' ' and
+            board[8] != ' ' and
+            board[9] != ' ')
 
-def place_marker():
+def player_choice(board):
     """
-    This one will add a marker into your desired position.
+    This one will ask you that where do you want to place your marker
     """
-    pass
+    pos = 0
+    while pos not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, pos):
+        pos = int(input('Choose a position 1-9 '))
 
-def win_check():
-    """
-    This one will check wether you won or not
-    """
-    pass
+    return pos
 
 def replay():
     """
     This one will ask you that you want to play again or not.
     """
-    pass
+    wanna = 'jyyfhv'
+    while wanna not in ['Y', 'N']:
+        wanna = input("Wanna play again(ans in Y or N) :")
+
+    return wanna == 'N'
 
 #Main
 print('Welcome to Tic Tac Toe!')
