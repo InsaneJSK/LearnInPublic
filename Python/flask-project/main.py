6ac1ac39034ae90fa174bf342db085fa7a8f94af
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-10-16 17:16:11 +0530

    LearnFlask-4

--- Python/flask-project/main.py
+++ Python/flask-project/main.py
@@ -1,25 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from market import app
 
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template("index.html")
-
-@app.route('/market')
-def market():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': 123456789012, 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': 345678901234, 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': 567890123456, 'price': 150}
-    ]
-    return render_template("market.html", items= items)
-
-"""
-Jinja Stuff to remember for future me:
-when you need to access the value of a variable stored and passed in python, in html, use this: {{ var }}
-for logic codes use {% logic %}
-eg for loop 
-{% for i in list_name %}
-{% endfor %}
-"""
+if __name__ == "__main__":
+    app.run(debug = True)
