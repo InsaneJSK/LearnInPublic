ac28b37b9256e6b5bee94dfe4d2d9841103b4ea3
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-10-17 14:02:10 +0530

    LearnFlask-6

--- Python/flask-project/market/__init__.py
+++ Python/flask-project/market/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = '3fcd98008c88147bfd0bec3a'
 
-from market import models
+db = SQLAlchemy(app)
 
 from market import routes
