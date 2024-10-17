87ecdedacfdd54a82774d10a6e06a96b5203660f
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-10-17 16:23:39 +0530

    LearnFlask-7

--- Python/flask-project/market/routes.py
+++ Python/flask-project/market/routes.py
@@ -1,6 +1,6 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
 
 @app.route('/')
@@ -13,7 +13,17 @@ def market():
     items = Item.query.all()
     return render_template("market.html", items= items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        newuser = User(username=form.username.data,
+                       email_address=form.email_address.data,
+                       password_hash=form.password1.data)
+        db.session.add(newuser)
+        db.session.commit()
+        return redirect(url_for('market'))
+    if form.errors != {}:
+        for i in form.errors.values():
+            print(i)
     return render_template('register.html', form = form)
