d14588251b2f1b2a6adfa539a75daecca300ffa7
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2024-10-19 23:58:15 +0530

    LearnFlask-9

--- Python/flask-project/market/routes.py
+++ Python/flask-project/market/routes.py
@@ -1,7 +1,7 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 
 @app.route('/')
 @app.route('/home')
@@ -19,7 +19,7 @@ def register():
     if form.validate_on_submit():
         newuser = User(username=form.username.data,
                        email_address=form.email_address.data,
-                       password_hash=form.password1.data)
+                       password=form.password1.data)
         db.session.add(newuser)
         db.session.commit()
         return redirect(url_for('market'))
@@ -27,3 +27,8 @@ def register():
         for i in form.errors.values():
             flash(f"There was an error with creating a user: {i}", category='danger')
     return render_template('register.html', form = form)
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form = form)
