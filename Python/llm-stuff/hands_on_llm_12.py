50627143480fdd1021c7f10389890c7aae538d42
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2026-02-09 23:51:12 +0530

    TinyLlama finetuning

--- Python/llm-stuff/hands_on_llm_12.py
+++ Python/llm-stuff/hands_on_llm_12.py
@@ -1,263 +1,113 @@
 # -*- coding: utf-8 -*-
-"""Hands-on-llm-chap11
+"""Hands-on-llm-chap12
 """
 
-# !pip install "datasets>=2.18.0,<3" transformers>=4.38.2 sentence-transformers>=2.5.1 accelerate>=0.27.2 seqeval>=1.2.2
+# !pip install -q accelerate==0.31.0 peft==0.11.1 bitsandbytes==0.43.1 transformers==4.41.2 trl==0.9.4 sentencepiece==0.2.0 triton==3.1.0
 
+from transformers import AutoTokenizer
 from datasets import load_dataset
-tomatoes = load_dataset("rotten_tomatoes")
-train_data, test_data = tomatoes["train"], tomatoes["test"]
 
-#Finetuning a pretrained bert
+template_tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-model_id = "bert-base-cased"
-model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+def format_prompt(example):
+    """Format the prompt to using the <|user|> template TinyLLama is using"""
+    chat = example["messages"]
+    prompt = template_tokenizer.apply_chat_template(chat, tokenize=False)
+    return {"text": prompt}
 
-from transformers import DataCollatorWithPadding
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-def preprocess_function(examples):
-   """Tokenize input data"""
-   return tokenizer(examples["text"], truncation=True)
-
-tokenized_train = train_data.map(preprocess_function, batched=True)
-tokenized_test = test_data.map(preprocess_function, batched=True)
-
-import numpy as np
-import evaluate
-
-
-def compute_metrics(eval_pred):
-    """Calculate F1 score"""
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-
-    load_f1 = evaluate.load("f1")
-    f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
-    return {"f1": f1}
-
-from transformers import TrainingArguments, Trainer
-training_args = TrainingArguments(
-   "model",
-   learning_rate=2e-5,
-   per_device_train_batch_size=16,
-   per_device_eval_batch_size=16,
-   num_train_epochs=1,
-   weight_decay=0.01,
-   save_strategy="epoch",
-   report_to="none"
+dataset = (
+    load_dataset("HuggingFaceH4/ultrachat_200k",  split="test_sft")
+      .shuffle(seed=42)
+      .select(range(3_000))
 )
-trainer = Trainer(
-   model=model,
-   args=training_args,
-   train_dataset=tokenized_train,
-   eval_dataset=tokenized_test,
-   data_collator=data_collator,
-   compute_metrics=compute_metrics,
+dataset = dataset.map(format_prompt)
+
+print(dataset["text"][10])
+
+#Quantization Models
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+model_name = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=True,
 )
 
-trainer.train()
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="auto",
+    quantization_config=bnb_config,
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
 
-trainer.evaluate()
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=False)
+tokenizer.pad_token = "<PAD>"
+tokenizer.padding_side = "left"
 
-#Freezing layers
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
-model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+peft_config = LoraConfig(
+    lora_alpha=32,
+    lora_dropout=0.1,
+    r=64,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=[
+        "k_proj", "gate_proj", "v_proj", "up_proj", "q_proj", "o_proj", "down_proj"
+    ]
+)
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, peft_config)
 
-for name, param in model.named_parameters():
-  print(name)
+from transformers import TrainingArguments
+output_dir = "./results"
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=4,
+    optim="paged_adamw_32bit",
+    learning_rate=2e-4,
+    lr_scheduler_type="cosine",
+    num_train_epochs=1,
+    logging_steps=10,
+    fp16=True,
+    gradient_checkpointing=True,
+)
 
-for name, param in model.named_parameters():
-  if name.startswith("classifier"):
-    param.requires_grad = True
-  else:
-    param.requires_grad = False
-
-from transformers import TrainingArguments, Trainer
-
-trainer = Trainer(
+from trl import SFTTrainer
+trainer = SFTTrainer(
     model=model,
-    args=training_args,
-    train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
-trainer.train()
-
-trainer.evaluate()
-
-model_id = "bert-base-cased"
-model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-for index, (name, param) in enumerate(model.named_parameters()):
-  if index<165:
-    param.requires_grad = False
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
-trainer.train()
-
-trainer.evaluate()
-
-# MLM
-from transformers import AutoModelForMaskedLM
-model = AutoModelForMaskedLM.from_pretrained("bert-base-cased")
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-
-def preprocess_function(examples):
-  return tokenizer(examples["text"], truncation=True)
-
-tokenized_train = train_data.map(preprocess_function, batched=True)
-tokenized_train = tokenized_train.remove_columns("label")
-tokenized_test = test_data.map(preprocess_function, batched=True)
-tokenized_test = tokenized_test.remove_columns("label")
-
-from transformers import DataCollatorForLanguageModeling
-data_collator = DataCollatorForLanguageModeling(
+    train_dataset=dataset,
+    dataset_text_field="text",
     tokenizer=tokenizer,
-    mlm=True,
-    mlm_probability=0.15,
+    args=training_arguments,
+    max_seq_length=512,
+    peft_config=peft_config,
 )
 
-training_args = TrainingArguments(
-    "model",
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=10,
-    weight_decay=0.01,
-    save_strategy="epoch",
-    report_to="none"
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
-    data_collator=data_collator,
-)
-
-tokenizer.save_pretrained("mlm")
 trainer.train()
-model.save_pretrained("mlm")
+trainer.model.save_pretrained("TinyLlama-1.1B-qlora")
+
+from peft import AutoPeftModelForCausalLM
+
+model = AutoPeftModelForCausalLM.from_pretrained(
+    "TinyLlama-1.1B-qlora",
+    low_cpu_mem_usage=True,
+    device_map=True,
+)
+merged_model = model.merge_and_unload()
 
 from transformers import pipeline
-mask_filler = pipeline("fill-mask", model="bert-base-cased")
-preds = mask_filler("What a horrible [MASK]!")
-for pred in preds:
-  print(f">>> {pred["sequence"]}")
 
-from transformers import pipeline
-mask_filler = pipeline("fill-mask", model="mlm")
-preds = mask_filler("What a horrible [MASK]!")
-for pred in preds:
-  print(f">>> {pred["sequence"]}")
+prompt = """<|user|>
+Tell me something about Large Language Models.</s>
+<|assistant|>
+"""
+pipe = pipeline(task="text-generation", model=merged_model, tokenizer=tokenizer)
+print(pipe(prompt)[0]["generated_text"])
 
-dataset = load_dataset("conll2003", trust_remote_code = True)
-
-example = dataset["train"][848]
-example
-
-label2id = {
-    'O': 0, 'B-PER': 1, 'I-PER': 2, 'B-ORG': 3, 'I-ORG': 4,
-    'B-LOC': 5, 'I-LOC': 6, 'B-MISC': 7, 'I-MISC': 8
-}
-id2label = {index: label for label, index in label2id.items()}
-label2id
-
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-model = AutoModelForTokenClassification.from_pretrained(
-    "bert-base-cased",
-    num_labels=len(id2label),
-    id2label=id2label,
-    label2id=label2id
-)
-
-token_ids = tokenizer(example["tokens"], is_split_into_words=True)["input_ids"]
-sub_tokens = tokenizer.convert_ids_to_tokens(token_ids)
-sub_tokens
-
-def align_labels(examples):
-    token_ids = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
-    labels = examples["ner_tags"]
-    updated_labels = []
-    for index, label in enumerate(labels):
-        word_ids = token_ids.word_ids(batch_index=index)
-        previous_word_idx = None
-        label_ids = []
-        for word_idx in word_ids:
-            if word_idx != previous_word_idx:
-                previous_word_idx = word_idx
-                updated_label = -100 if word_idx is None else label[word_idx]
-                label_ids.append(updated_label)
-            elif word_idx is None:
-                label_ids.append(-100)
-            else:
-                updated_label = label[word_idx]
-                if updated_label % 2 == 1:
-                    updated_label += 1
-                label_ids.append(updated_label)
-        updated_labels.append(label_ids)
-    token_ids["labels"] = updated_labels
-    return token_ids
-tokenized = dataset.map(align_labels, batched=True)
-
-print(f"Original: {example['ner_tags']}")
-print(f"Updated: {tokenized['train'][848]['labels']}")
-
-import evaluate
-
-seqeval = evaluate.load("seqeval")
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=2)
-    true_predictions = []
-    true_labels = []
-    for prediction, label in zip(predictions, labels):
-      for token_prediction, token_label in zip(prediction, label):
-        if token_label != -100:
-          true_predictions.append([id2label[token_prediction]])
-          true_labels.append([id2label[token_label]])
-    results = seqeval.compute(predictions=true_predictions, references=true_labels)
-    return {"f1": results["overall_f1"]}
-
-data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
-
-training_args = TrainingArguments(
-   "model",
-   learning_rate=2e-5,
-   per_device_train_batch_size=16,
-   per_device_eval_batch_size=16,
-   num_train_epochs=1,
-   weight_decay=0.01,
-   save_strategy="epoch",
-   report_to="none"
-)
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized["train"],
-    eval_dataset=tokenized["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
-trainer.train()
-
-trainer.evaluate()
-
-trainer.save_model("ner_model")
-token_classifier = pipeline(
-    "token-classification",
-    model="ner_model",
-)
-token_classifier("My name is Jaspreet.")
