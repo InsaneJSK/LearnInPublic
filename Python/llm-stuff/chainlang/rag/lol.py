ce81b6272e76cb45f354b9cfaa56758de561b508
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2026-03-06 00:28:47 +0530

    clean-up

--- Python/llm-stuff/chainlang/rag/lol.py
+++
@@ -1,8 +0,0 @@
-## Vector Embedding And Vector Store
-from langchain.embeddings import HuggingFaceEmbeddings
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-print(type(embeddings))
