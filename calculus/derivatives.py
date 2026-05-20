a4a0e8feec0b28e5707bd04775ddb8402f13857c
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2026-05-20 23:41:25 +0530

    implicit differentiation

--- calculus/derivatives.py
+++ calculus/derivatives.py
@@ -85,4 +85,39 @@ eq.diff(x)
 # %%
 eq.diff(x).subs(x, 2)
 
+# %% [markdown]
+# ### Implicit differentiation
+
+# %%
+import sympy as sp
+x, y = sp.symbols('x y')
+eq = x**2 + y**2 - 25
+derivative = sp.idiff(eq, y, x)
+eq
+
+# %%
+derivative
+
+# %%
+sp.plot_implicit(eq)
+
+# %%
+x_val, y_val = 3, 4
+ans_x = derivative.subs({x: x_val, y: y_val})
+ans_y = ans_x.subs(y, y_val)
+print(ans_y)
+
+# %%
+x_val = 3
+x_part = eq.subs(x, x_val)
+y_part = sp.Eq(x_part, 0)
+y_val = sp.solve(y_part, y)
+print(f"At x = {x_val}, y can be: {y_val}")
+
+# %%
+for y_loop in y_val:
+    ans_x = derivative.subs({x: x_val})
+    ans_y = ans_x.subs(y, y_loop)
+    print(f"At x = {x_val} and y = {y_loop}, the slope of the tangent line is: {ans_y}")
+
 # %%
